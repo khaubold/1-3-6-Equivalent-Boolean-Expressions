4749f4f7bbf449e08d0b0ae6d6f3debd62503eac
Kristen Haubold <khaubold@sbcsc.k12.in.us>
2023-11-02 13:28:08 -0400

    Add files via upload

---
+++ DeMorgansLaw.java
@@ -0,0 +1,22 @@
+/*
+ * Activity 1.3.6
+ */
+import java.util.Scanner;
+
+public class DeMorgansLaw
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter 'true' or 'false' for A");
+    boolean a = Boolean.parseBoolean(sc.nextLine()); 
+
+    System.out.println("Enter 'true' or 'false' for B");
+    boolean b = Boolean.parseBoolean(sc.nextLine());
+    
+    /* your code here */
+    
+    
+  }
+}
